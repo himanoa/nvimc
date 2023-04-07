445d5fd998146c38cb264aeca17aebea4832fe8d
himanoa <matsunoappy@gmail.com>
2023-04-08 05:48:44 +0900

    deno or tserver

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -130,10 +130,7 @@ return require('packer').startup(function(use)
   use {'junkblocker/patchreview-vim'}
   use {'chemzqm/denite-git'}
   use { 'neovim/nvim-lspconfig' }
-  use { "williamboman/mason.nvim", config = function()
-    local mason = require("mason-lspconfig")
-    mason.setup()
-  end}
+  use { "williamboman/mason.nvim"}
   use {"williamboman/mason-lspconfig.nvim"}
   use {'himanoa/goshiteki'}
   use {'ruanyl/vim-gh-line'}
@@ -299,25 +296,63 @@ return require('packer').startup(function(use)
     group = nvim_metals_group,
   })
 
-  require("mason-lspconfig").setup_handlers {
-    function (server_name) -- default handler (optional)
-      require("lspconfig")[server_name].setup {
-        on_attach = on_attach
+
+  local mason = require('mason')
+  local mason_lspconfig = require('mason-lspconfig')
+  local nvim_lsp = require('lspconfig')
+  mason_lspconfig.setup_handlers({
+  function(server_name)
+    local node_root_dir = nvim_lsp.util.root_pattern("package.json")
+    local is_node_repo = node_root_dir(vim.api.nvim_buf_get_name(0)) ~= nil
+
+    local opts = {}
+
+    opts.on_attach = function() end
+
+    if server_name == "tsserver" then
+      if not is_node_repo then
+        return
+      end
+
+
+      opts.init_options = {
+        hostInfo = "neovim",
+        preferences = {
+          importModuleSpecifierPreference = "non-relative"
+        }
+      }
+      opts.root_dir = node_root_dir
+    elseif server_name == "eslint" then
+      if not is_node_repo then
+        return
+      end
+
+      opts.root_dir = node_root_dir
+    elseif server_name == "denols" then
+      if is_node_repo then
+        return
+      end
+
+      opts.root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc", "deps.ts", "import_map.json")
+      opts.init_options = {
+        lint = true,
+        unstable = true,
+        suggest = {
+          imports = {
+            hosts = {
+              ["https://deno.land"] = true,
+              ["https://cdn.nest.land"] = true,
+              ["https://crux.land"] = true
+            }
+          }
+        }
       }
     end
-  }
 
-  require('lspconfig')['tsserver'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-    cmd = { "typescript-language-server", "--stdio" },
-    init_options= {
-      hostInfo = "neovim",
-      preferences = {
-        importModuleSpecifierPreference = "non-relative"
-      }
-    }
-  }
+    nvim_lsp[server_name].setup(opts)
+  end
+})
+
   if packer_bootstrap then
     require('packer').sync()
   end
