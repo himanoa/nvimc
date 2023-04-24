ce29376de7bccdd444ecd53d9a33a25ebbcf58cf
himanoa <matsunoappy@gmail.com>
2023-04-25 04:45:45 +0900

    markdownをいい感じに書くための設定を追加

--- lua/filetypes/markdown.lua
+++ lua/filetypes/markdown.lua
@@ -2,6 +2,8 @@ local mod = {
   load = function()
     vim.opt_local.comments = {'b:*','b:-','b:+','b:1.','nb:>'}
     vim.opt_local.formatoptions = "rqnljro"
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
   end
 }
 
